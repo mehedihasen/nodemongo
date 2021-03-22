88370dc08e82a98d139fafc86dbaed3b82c2e2a7
mehedihasen <mehedisabuj4@gmail.com>
2021-03-23 04:44:28 +0800

    add some

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express')
 var app = express()
-app.get('/', (req, res) => res.send("wellcome to nodemongo") )
+const users =["hasid", "Rakib", "Kudds", 'almin', 'saide', 'Khalied']
+
+app.get("/users/:id", (req, res) => {
+    const id = req.params.id;
+    const name = users[id];
+    res.send({id, name});
+})
 
 app.listen(3000, ()=> console.log("Listening to port 3000"))
